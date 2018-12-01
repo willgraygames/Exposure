38ef64285631b7d6c1ceb1fa02d7a742a07e3831
William Gray <willgraygames@gmail.com>
2018-12-01 01:11:23 -0500

    PlayerInteractivity

---
+++ Exposure/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public string itemName;                     //The name of the item
+    public int fireValue;                       //The value this item adds to the fire if burned
+    public int pitvalue;                        //The value this item adds to the pit if sacrificed
+    public bool burnable;                       //Whether or not this object is burned in the fire
+
+    void Start ()
+    {
+        gameObject.name = itemName;
+    }
+}
